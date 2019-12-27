9cacce7f4800243ccd1e70cc43089b6ee5ffdaab
Rakesh <rakeshmohanta2k10@gmail.com>
2019-12-27 16:45:34 +0530

    Updates

---
+++ precache-manifest.0fdf3ca2c18b7e7bff899d0fb1f5ffc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9c63bb21539d9970be50281b72b384",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "7848cbe82aaad524a26e",
+    "url": "/react-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dd37737972c9b35b7987",
+    "url": "/react-app/static/js/2.dce32cb9.chunk.js"
+  },
+  {
+    "revision": "251965ec84181576ff1502cd65084d13",
+    "url": "/react-app/static/js/2.dce32cb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7848cbe82aaad524a26e",
+    "url": "/react-app/static/js/main.6a9640b5.chunk.js"
+  },
+  {
+    "revision": "9cc736b3583ddc2ce5bf",
+    "url": "/react-app/static/js/runtime-main.65be1f42.js"
+  }
+]);
